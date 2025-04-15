38706cf80a5622eb1566d4e054afa0f22973b62e
Efim Timofeev <139679017+Fima1716@users.noreply.github.com>
2025-04-15 20:42:48 +0300

    Add files via upload

---
+++ VinylList.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import VinylCard from "./VinylCard.vue";
+import { defineComponent } from "vue";
+import type { PropType } from "vue";
+
+interface VinylItem {
+  id: number;
+  name: string;
+  price: number;
+  condition: string;
+  year: number;
+  imageUrl: string;
+}
+
+export default defineComponent({
+  name: "VinylList",
+  components: { VinylCard },
+  props: {
+    items: {
+      type: Array as PropType<VinylItem[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="grid grid-cols-3 gap-5">
+    <VinylCard
+      v-for="item in items"
+      :key="item.id"
+      :image-url="item.imageUrl"
+      :album-name="item.name"
+      :release-date="item.year"
+      :condition="item.condition"
+      :vinyl-price="item.price"
+    />
+  </div>
+</template>
